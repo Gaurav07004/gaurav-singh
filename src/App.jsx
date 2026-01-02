43d18feeea9b0bc706e9a6480a5f8e600f002713
Gaurav07004 <singhgaurav07004@gmail.com>
2026-01-02 19:26:07 +0530

    Working on responsiveness for portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,8 @@ import AppRouter from "./router";
 
 export default function App() {
   useEffect(() => {
-    if ("scrollRestoration" in window.history) {
-      window.history.scrollRestoration = "manual";
-    }
-
     window.scrollTo(0, 0);
+    window.history.replaceState(null, "", "/");
   }, []);
 
   return (
